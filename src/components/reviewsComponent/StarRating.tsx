bc698925759195ea7259bf3496cd2fcda0c1d7a2
rkeerthient <rkeerthi@yext.com>
2025-11-11 14:10:15 +0530

    replaced blue

--- src/components/reviewsComponent/StarRating.tsx
+++ src/components/reviewsComponent/StarRating.tsx
@@ -5,7 +5,7 @@ const StarRating = ({ selectedStars = 3 }: any) => {
   const firstMethod = () => {
     return [...Array(totalStars)].map((el, i: any) =>
       i < selectedStars ? (
-        <i key={i} className="fa fa-star text-[#1F66E0]" />
+        <i key={i} className="fa fa-star text-[#0176a0]" />
       ) : (
         <i key={i} className="fa fa-star-o" />
       )
@@ -15,9 +15,9 @@ const StarRating = ({ selectedStars = 3 }: any) => {
   const secondMethod = () => {
     return [...Array(totalStars)].map((el, i: any) =>
       i < selectedStars && i + 1 > selectedStars ? (
-        <i key={i} className="fa fa-star-half-o text-[#1F66E0]" />
+        <i key={i} className="fa fa-star-half-o text-[#0176a0]" />
       ) : i < selectedStars ? (
-        <i key={i} className="fa fa-star text-[#1F66E0]" />
+        <i key={i} className="fa fa-star text-[#0176a0]" />
       ) : (
         <i key={i} className="fa fa-star-o" />
       )
